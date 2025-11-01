c1945c7020bdf00c2f5686f6282902ce01e08680
landeralvin <alvin.lander@mandirisekuritas.co.id>
2025-11-01 19:26:57 +0700

    initalize redis cluster

--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -5,16 +5,20 @@ import (
 )
 
 type RedisClient struct {
-	client *redis.Client
+	Client        *redis.Client
+	ClientCluster *redis.ClusterClient
 }
 
 type RedisClientConfig struct {
-	Addr     string
-	Password string
-	DB       int
+	Addr        string
+	AddrCluster []string
+	Password    string
+	DB          int
 }
 
 type Option func(*redis.Options)
+
+type OptionCluster func(*redis.ClusterOptions)
 
 func ConnectWithOption(cfg RedisClientConfig) Option {
 	return func(o *redis.Options) {
@@ -32,12 +36,38 @@ func ConnectWithOption(cfg RedisClientConfig) Option {
 	}
 }
 
+func ConnectWithOptionCluster(cfg RedisClientConfig) OptionCluster {
+	return func(o *redis.ClusterOptions) {
+		if len(cfg.AddrCluster) == 0 {
+			cfg.AddrCluster = []string{"localhost:6379"}
+		}
+
+		if cfg.Password == "" {
+			cfg.Password = ""
+		}
+
+		if cfg.DB == 0 {
+			cfg.DB = 0
+		}
+	}
+}
+
+func NewRedisClusterClient(opts ...OptionCluster) *RedisClient {
+	clusterOptions := &redis.ClusterOptions{}
+	for _, opt := range opts {
+		opt(clusterOptions)
+	}
+	return &RedisClient{
+		ClientCluster: redis.NewClusterClient(clusterOptions),
+	}
+}
+
 func NewRedisClient(opts ...Option) *RedisClient {
 	options := &redis.Options{}
 	for _, opt := range opts {
 		opt(options)
 	}
 	return &RedisClient{
-		client: redis.NewClient(options),
+		Client: redis.NewClient(options),
 	}
 }
--- pkg/redis/redis_interface.go
+++ pkg/redis/redis_interface.go
@@ -17,48 +17,103 @@ type RedisInterface interface {
 	HGetAll(ctx context.Context, key string) (map[string]string, error)
 	HDel(ctx context.Context, key string, fields ...string) error
 	HGet(ctx context.Context, key string, field string) (string, error)
+	GetCluster(ctx context.Context, key string) (string, error)
+	SetCluster(ctx context.Context, key string, value interface{}, expiration time.Duration) error
+	DelCluster(ctx context.Context, keys ...string) error
+	ExistsCluster(ctx context.Context, keys ...string) (int64, error)
+	PingCluster(ctx context.Context) (string, error)
+	CloseCluster(ctx context.Context) error
+	HMSetCluster(ctx context.Context, key string, values map[string]interface{}) error
+	HMGetCluster(ctx context.Context, key string, fields ...string) ([]interface{}, error)
+	HGetAllCluster(ctx context.Context, key string) (map[string]string, error)
+	HDelCluster(ctx context.Context, key string, fields ...string) error
+	HGetCluster(ctx context.Context, key string, field string) (string, error)
 }
 
 func (r *RedisClient) Get(ctx context.Context, key string) (string, error) {
-	return r.client.Get(ctx, key).Result()
+	return r.Client.Get(ctx, key).Result()
 }
 
 func (r *RedisClient) Set(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
-	return r.client.Set(ctx, key, value, expiration).Err()
+	return r.Client.Set(ctx, key, value, expiration).Err()
 }
 
 func (r *RedisClient) Del(ctx context.Context, keys ...string) error {
-	return r.client.Del(ctx, keys...).Err()
+	return r.Client.Del(ctx, keys...).Err()
 }
 
 func (r *RedisClient) Exists(ctx context.Context, keys ...string) (int64, error) {
-	return r.client.Exists(ctx, keys...).Result()
+	return r.Client.Exists(ctx, keys...).Result()
 }
 
 func (r *RedisClient) Ping(ctx context.Context) (string, error) {
-	return r.client.Ping(ctx).Result()
+	return r.Client.Ping(ctx).Result()
 }
 
 func (r *RedisClient) Close(ctx context.Context) error {
-	return r.client.Close()
+	return r.Client.Close()
 }
 
 func (r *RedisClient) HMSet(ctx context.Context, key string, values map[string]interface{}) error {
-	return r.client.HMSet(ctx, key, values).Err()
+	return r.Client.HMSet(ctx, key, values).Err()
 }
 
 func (r *RedisClient) HMGet(ctx context.Context, key string, fields ...string) ([]interface{}, error) {
-	return r.client.HMGet(ctx, key, fields...).Result()
+	return r.Client.HMGet(ctx, key, fields...).Result()
 }
 
 func (r *RedisClient) HGetAll(ctx context.Context, key string) (map[string]string, error) {
-	return r.client.HGetAll(ctx, key).Result()
+	return r.Client.HGetAll(ctx, key).Result()
 }
 
 func (r *RedisClient) HDel(ctx context.Context, key string, fields ...string) error {
-	return r.client.HDel(ctx, key, fields...).Err()
+	return r.Client.HDel(ctx, key, fields...).Err()
 }
 
 func (r *RedisClient) HGet(ctx context.Context, key string, field string) (string, error) {
-	return r.client.HGet(ctx, key, field).Result()
+	return r.Client.HGet(ctx, key, field).Result()
+}
+
+func (r *RedisClient) GetCluster(ctx context.Context, key string) (string, error) {
+	return r.ClientCluster.Get(ctx, key).Result()
+}
+
+func (r *RedisClient) SetCluster(ctx context.Context, key string, value interface{}, expiration time.Duration) error {
+	return r.ClientCluster.Set(ctx, key, value, expiration).Err()
+}
+
+func (r *RedisClient) DelCluster(ctx context.Context, keys ...string) error {
+	return r.ClientCluster.Del(ctx, keys...).Err()
+}
+
+func (r *RedisClient) ExistsCluster(ctx context.Context, keys ...string) (int64, error) {
+	return r.ClientCluster.Exists(ctx, keys...).Result()
+}
+
+func (r *RedisClient) PingCluster(ctx context.Context) (string, error) {
+	return r.ClientCluster.Ping(ctx).Result()
+}
+
+func (r *RedisClient) CloseCluster(ctx context.Context) error {
+	return r.ClientCluster.Close()
+}
+
+func (r *RedisClient) HMSetCluster(ctx context.Context, key string, values map[string]interface{}) error {
+	return r.ClientCluster.HMSet(ctx, key, values).Err()
+}
+
+func (r *RedisClient) HMGetCluster(ctx context.Context, key string, fields ...string) ([]interface{}, error) {
+	return r.ClientCluster.HMGet(ctx, key, fields...).Result()
+}
+
+func (r *RedisClient) HGetAllCluster(ctx context.Context, key string) (map[string]string, error) {
+	return r.ClientCluster.HGetAll(ctx, key).Result()
+}
+
+func (r *RedisClient) HDelCluster(ctx context.Context, key string, fields ...string) error {
+	return r.ClientCluster.HDel(ctx, key, fields...).Err()
+}
+
+func (r *RedisClient) HGetCluster(ctx context.Context, key string, field string) (string, error) {
+	return r.ClientCluster.HGet(ctx, key, field).Result()
 }
